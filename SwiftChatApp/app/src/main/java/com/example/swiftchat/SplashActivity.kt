c9748350143a950d80554a2d55ffa348a3542cce
raqeebAKhan <ritzyraqeeb@gmail.com>
2021-05-29 17:28:38 +0530

    Splash activity added along with drawable and animation

---
+++ SwiftChatApp/app/src/main/java/com/example/swiftchat/SplashActivity.kt
@@ -0,0 +1,47 @@
+package com.example.swiftchat
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.view.animation.AnimationUtils
+import kotlinx.android.synthetic.main.activity_splash.*
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        val runnable = Runnable {
+            val splashIntent = Intent(this, MainActivity::class.java)
+            startActivity(splashIntent)
+            finish()
+        }
+        val handler = Handler(Looper.getMainLooper())
+        handler.postDelayed(runnable, 3400)
+
+        animationOnSplash()
+    }
+
+    private fun animationOnSplash() {
+        val splashAnimation = AnimationUtils.loadAnimation(
+            this,
+            R.anim.transitions_splash
+        )
+
+        val lTRTextAnimation = AnimationUtils.loadAnimation(
+            this,
+            R.anim.left_to_right
+        )
+
+        val lTRIconAnimation = AnimationUtils.loadAnimation(
+            this,
+            R.anim.left_to_right
+        )
+
+        ivLogo.animation = splashAnimation
+        tvAppName.animation = lTRTextAnimation
+        ivChatIcon.animation = lTRIconAnimation
+    }
+}
